69990cfe98dbdcec3935c86ff0ccb62a38c42534
shin520 <adtiennguyen21@gmail.com>
2021-12-03 10:24:24 +0700

    chinh sua giao dien 4

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -11,24 +11,26 @@
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
   <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,400;0,500;0,600;0,700;1,800;1,900&display=swap" rel="stylesheet">
 </head>
-<div class="container header">
-  <nav>
-      <ul>
-          <li>
-              <a href="{{route('index')}}"><img src="{{asset('assets')}}/img/logo.png" alt=""></a>
-          </li>
-          <li>
-              <a href="{{route('get_payment')}}">PAYMENT</a>
-          </li>
-          <li>
-              <a href="{{route('get_transactions')}}">TRANSACTIONS</a>
-          </li>
-          <li>
-              <a href="{{route('get_blocks')}}">BLOCK</a>
-          </li>
-          
-      </ul>
-  </nav>
+<div class="header">
+  <div class="container header-data">
+      <nav>
+          <ul>
+              <li>
+                  <a href="{{route('index')}}"><img src="{{asset('assets')}}/img/logo.png" alt=""></a>
+              </li>
+              <li>
+                  <a href="{{route('get_payment')}}">PAYMENT</a>
+              </li>
+              <li>
+                  <a href="{{route('get_transactions')}}">TRANSACTIONS</a>
+              </li>
+              <li>
+                  <a href="{{route('get_blocks')}}">BLOCK</a>
+              </li>
+      
+          </ul>
+      </nav>
+  </div>
   {{-- <div class="x">
       <a class="x-btn sMgR20" href="">TESTNET</a>
       <div class="lang">LANG
--- resources/views/load/load-payment.blade.php
+++ resources/views/load/load-payment.blade.php
@@ -23,7 +23,7 @@
         <a href="">{{ \Illuminate\Support\Str::limit($d['source_account'], 4, $end='') }}</a>
     </td>
     @if($d['type']=='payment')
-    <td>Pay<span class="pi-coin">{{floatval($d['amount'])}}π</span>  to <a class="name" href=""> {{ \Illuminate\Support\Str::limit($d['to'], 4, $end='') }}</a></td>
+    <td>Pay <span class="pi-coin">{{floatval($d['amount'])}}π</span>  to <a class="name" href=""> {{ \Illuminate\Support\Str::limit($d['to'], 4, $end='') }}</a></td>
     @else
     <td>Created Account <a class="name" href=""> {{ \Illuminate\Support\Str::limit($d['account'], 4, $end='') }}</a> with balance
         <span class="pi-coin">{{floatval($d['starting_balance'])}}π</span>   </td>
