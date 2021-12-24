0c49510519197b80a9327a029a10d39a2ae8ffd1
maiphucduy147 <nmtran68@gmail.com>
2021-12-24 13:57:17 +0700

    ok

--- resources/views/layouts/seo.blade.php
+++ resources/views/layouts/seo.blade.php
@@ -11,7 +11,7 @@
 <meta name="google" content="nositelinkssearchbox" />
 <meta name="author" content="Pinetwork.Trade | Pi Blockchain Explorer">
 <meta name="copyright" content="Pinetwork.Trade | Pi Blockchain Explorer [contact.pinetworktrade@gmail.com]">
-<meta name="keywords" content="Pinetwork | Pi Blockchain Explorer | Pi Trade | Minepi | Pinetwork.trade | Sàn Pinetwork | Giá Pinetwork | Pinetwork Nodes | Pinetwork Refernal Code | Giá tiền Pinetwork | Giao dịch Pi | Pi có giá trị không | Pi lừa đảo | Mua Pi | Bán Pi | Thu gom pi | Mua bán Pi giá rẻ">
+<meta name="keywords" content="Pinetwork, Pi Blockchain Explorer, Pi Trade, Minepi, Pinetwork.trade, Sàn Pinetwork, Giá Pinetwork, Pinetwork Nodes,  Pinetwork Refernal Code,  Giá tiền Pinetwork,  Giao dịch Pi,  Pi có giá trị không,  Pi lừa đảo,  Mua Pi,  Bán Pi  Thu gom pi,  Mua bán Pi giá rẻ, Pi Network,Pi network,Pi coin,picoin,pi network,gia pi,giá pi,giá pi network,KYC,kyc,kyc là gì,kyc la gi,Gia pi network,mua bán trao đổi Pi Network,Cộng đồng Pi Network,mua bán Pi Network,mua pi,bán pi,mua bán Pi,mua ban pi,blockchain Pi,mainnet,pi node">
 <!-- Global site tag (gtag.js) - Google Analytics -->
 <script async src="https://www.googletagmanager.com/gtag/js?id=G-98HY55KKVJ"></script>
 <script>
